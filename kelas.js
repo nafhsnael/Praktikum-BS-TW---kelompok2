7b758bbdf5f0a31f9da2d931947fab975f9cf1ec
Firyal zalfaa A <zalfaafiryal3@gmail.com>
2026-03-25 08:55:29 +0700

    Update kelas.js

--- kelas.js
+++ kelas.js
@@ -2,10 +2,12 @@
 function getKelas() {
     let kelas = document.getElementById("kelas").value.trim();
 
-    if(kelas === ""){
-        alert("Kelas belum diisi 😠");
+    if (kelas === "") {
+        alert("Kelas belum diisi!");
+        console.log("Input kelas kosong");
         return null;
     }
 
+    console.log("Kelas:", kelas);
     return kelas;
 }
